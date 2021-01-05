b4930a988dceaa8ed2338e9590d2d75e8962f4d6
abrar-nazib@github.com <abrarnazib@gmail.com>
2021-01-05 17:35:48 +0600

    finished coding my first window

--- benny/include/display.h
+++ benny/include/display.h
@@ -7,12 +7,18 @@ class Display
 public:
     Display(int width, int height, const std::string& title);
     virtual ~Display();
+    void Update(); //SwapBuffer
+    bool IsClosed();
+    void ClearSrceen(float r, float g, float b, float x);
 
 protected:
 
 private:
     Display(const Display& other) {}
     Display& operator=(const Display& other) {}
+    SDL_Window* m_window;
+    SDL_GLContext m_glContext;
+    bool m_isClosed;
 };
 
 #endif // DISPLAY_H
--- benny/main.cpp
+++ benny/main.cpp
@@ -2,8 +2,13 @@
 #include <GL/glew.h>
 #include "display.h"
 
+
 int main()
 {
-    Display display(800, 600, "Hello world");
+    Display display(800, 600, "My First Window");
+    while (!display.IsClosed()){
+        display.ClearSrceen(0.0f, 0.1f, 0.1f, 1.0f);
+        display.Update();
+    }
     return 0;
 }
